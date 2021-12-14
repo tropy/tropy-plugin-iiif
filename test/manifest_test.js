0388a73fdecff3f076f39a801ff45ca8866e55a9
Sylvester Keil <sylvester@keil.or.at>
2021-12-14 12:45:30 +0100

    Extract core props from manifest

--- test/manifest_test.js
+++ test/manifest_test.js
@@ -3,13 +3,13 @@ const jsonld = require('jsonld')
 const { Manifest } = require('../lib/manifest')
 
 describe('IIIF Manifest', () => {
+
   describe('.parse()', () => {
     it('accepts v2 manifests', async () => {
-      let [mdz] = await Manifest.parse(F.json('mdz'), jsonld)
-      assert.ok(mdz instanceof Manifest)
-
-      let [bodleian] = await Manifest.parse(F.json('bodleian'), jsonld)
-      assert.ok(bodleian instanceof Manifest)
+      await assert.doesNotReject(
+        Manifest.parse({
+          '@type': 'http://iiif.io/api/presentation/2#Manifest'
+        }, jsonld))
     })
 
     it('ignores missing input', async () => {
@@ -22,10 +22,26 @@ describe('IIIF Manifest', () => {
         '@type': 'http://iiif.io/api/presentation/3#Manifest'
       }, jsonld))
 
-      await assert.doesNotReject(
-        Manifest.parse({
-          '@type': 'http://iiif.io/api/presentation/2#Manifest'
-        }, jsonld))
     })
   })  
+
+  describe('fixtures', () => {
+    let bodleian, mdz
+
+    before(async () => {
+      bodleian = (await Manifest.parse(F.json('bodleian'), jsonld))[0]
+      mdz = (await Manifest.parse(F.json('mdz'), jsonld))[0]
+    })
+
+    it('are manifest instances', () => {
+      assert.ok(bodleian instanceof Manifest)
+      assert.ok(mdz instanceof Manifest)
+    })
+
+    it('have core props', () => {
+      assert.ok('http://purl.org/dc/elements/1.1/title' in bodleian.props)
+      assert.ok('http://purl.org/dc/elements/1.1/rights' in mdz.props)
+    })
+  })
+
 })
