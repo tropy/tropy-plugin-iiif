2280bc38a86d23c0492067e197ca1744c9ae715c
Martim Passos <49573683+martimpassos@users.noreply.github.com>
2024-09-11 15:01:27 -0300

    v3 tests

--- test/manifest_test.js
+++ test/manifest_test.js
@@ -17,33 +17,36 @@ describe('IIIF Manifest', () => {
       await assert.equal(0, (await Manifest.parse({}, jsonld)).length)
     })
 
-    it('throws for unsupported types', async () => {
-      await assert.rejects(Manifest.parse({
-        '@type': 'http://iiif.io/api/presentation/3#Manifest'
-      }, jsonld))
+    // it('throws for unsupported types', async () => {
+    //   await assert.rejects(Manifest.parse({
+    //     '@type': 'http://iiif.io/api/presentation/3#Manifest'
+    //   }, jsonld))
 
-    })
+    // })
   })
 
   describe('fixtures', () => {
-    let bodleian, mdz, loc
+    let bodleian, mdz, loc, northwestern
 
     before(async () => {
       bodleian = (await Manifest.parse(F.json('bodleian'), jsonld))[0]
       mdz = (await Manifest.parse(F.json('mdz'), jsonld))[0]
       loc = (await Manifest.parse(F.json('loc'), jsonld))[0]
+      northwestern = (await Manifest.parse(F.json('northwestern'), jsonld))[0]
     })
 
     it('are manifest instances', () => {
       assert.ok(bodleian instanceof Manifest)
       assert.ok(mdz instanceof Manifest)
       assert.ok(loc instanceof Manifest)
+      assert.ok(northwestern instanceof Manifest)
     })
 
     it('have core props', () => {
       assert.ok('http://purl.org/dc/elements/1.1/title' in bodleian.props)
       assert.ok('http://purl.org/dc/elements/1.1/rights' in mdz.props)
       assert.ok('http://purl.org/dc/elements/1.1/rights' in loc.props)
+      assert.ok('http://purl.org/dc/elements/1.1/rights' in northwestern.props)
     })
 
     it('skips blank props', () => {
@@ -54,12 +57,14 @@ describe('IIIF Manifest', () => {
       assert.ok(bodleian.canvases.every(c => c instanceof Canvas))
       assert.ok(mdz.canvases.every(c => c instanceof Canvas))
       assert.ok(loc.canvases.every(c => c instanceof Canvas))
+      assert.ok(northwestern.canvases.every(c => c instanceof Canvas))
     })
 
     it('have images', () => {
       assert.ok(bodleian.images.map(i => i instanceof Image))
       assert.ok(mdz.images.every(i => i instanceof Image))
       assert.ok(loc.images.every(i => i instanceof Image))
+      assert.ok(northwestern.images.every(i => i instanceof Image))
     })
 
     it('prop values are stripped of html', () => {
